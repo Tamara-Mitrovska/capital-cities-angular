c32fbf36c75656932db9da664bfee4af0f07c2f7
Tamara Mitrovska <tamaram@mit.edu>
2024-12-28 17:14:29 +0100

    map code clean up

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -12,25 +12,21 @@ export class MapComponent implements AfterViewInit {
   
   chartType = ChartType.GeoChart;
 
-  chartData = [ // country, color
-    ["United States", 0],
-    ["Germany", 100]
+  chartData = [
+    ["Germany", 0],
+    ["United States", 100],
   ];
 
   chartOptions = {
-    region: 'world',  // use codes like 150 for Europe, or 'world' for all
-    displayMode: 'regions',  // Show regions (countries)
-    colorAxis: { colors: ['red', 'green'] },  // Color range for regions based on value (0-100) from chartData list
-    
-    backgroundColor: '#f0f0f0',  // Background color for the chart
-    datalessRegionColor: '#f5f5f5',  // Color for regions with no data
-    defaultColor: '#f5f5f5'  // Default color for regions
+    region: 'world',
+    displayMode: 'regions',
+    colorAxis: { colors: ['red', 'green'] },
   }
 
   ngAfterViewInit(): void {
     if (this.googleChart) {
       this.googleChart.ready.subscribe(_ => {
-        const chart = this.googleChart.chartWrapper.getChart();  // Access the chart instance
+        const chart = this.googleChart.chartWrapper.getChart();
         google.visualization.events.addListener(chart, 'select', () => this.onSelect(chart));
       })
     }
@@ -40,9 +36,7 @@ export class MapComponent implements AfterViewInit {
     const selection = chart.getSelection();
     if (selection.length > 0) {
       const selectedItem = selection[0];
-      const countryName = this.chartData[selectedItem.row][0];
-      const population = this.chartData[selectedItem.row][1];
-      console.log(`You clicked on ${countryName}.`);
+      const country = this.chartData[selectedItem.row][0];
     }
   }
 }
