81d581ea1ff85878035ff5566d555279c228d696
Tamara Mitrovska <tamaram@mit.edu>
2024-12-28 18:52:40 +0100

    get all countries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoogleChartsModule } from 'angular-google-charts';
 import { MapComponent } from './map/map.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MapComponent } from './map/map.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GoogleChartsModule
+    GoogleChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,26 +1,42 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ChartType, GoogleChartComponent, GoogleChartsConfig } from 'angular-google-charts';
+import { MapService } from './map.service';
+import { Country } from './map.domain';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements AfterViewInit {
+export class MapComponent implements AfterViewInit, OnInit {
 
   @ViewChild('googleChart', {static: true}) googleChart!: GoogleChartComponent;
   
   chartType = ChartType.GeoChart;
 
   chartData = [
-    ["Germany", 0],
-    ["United States", 100],
+    ["Russia", 0],
+    ["Germany", 1],
+    ["United States", 2],
   ];
 
   chartOptions = {
     region: 'world',
     displayMode: 'regions',
-    colorAxis: { colors: ['red', 'green'] },
+    colorAxis: { colors: ['grey', 'red', 'green'], values: [0, 1, 2] },
+    legend: 'none',
+    tooltip: {}
+  }
+
+  countries: Country[] = [];
+
+  constructor(private mapService: MapService) {}
+
+  ngOnInit(): void {
+    this.mapService.getAllCounties().subscribe(countries => {
+      this.countries = countries;
+      this.chartData = countries.map(c => [c.code, 0])
+    })
   }
 
   ngAfterViewInit(): void {
